5392925d6d63104f3aa26a1f5b29f4b06be52e0b
Pablo Hernandez <pablohernandezcontacto@gmail.com>
2021-04-10 03:39:14 +0200

    Busqueda asincrona
    Ya funciona busqueda asincrona.
     Hay que encontrar el bug de que algunas url se repiten con y sin www. lo que hace que se visiten varias veces, aunque habria que ver si las web con y sin url tienen el mismo contenido ya que podrian tener links diferentes.
    
    Hay que hacer el export, recordatorio: tengo todo en notepad++

--- CRAWLER.py
+++ CRAWLER.py
@@ -1,36 +1,92 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 31 21:03:12 2021
+https://github.com/safteinzz/WebCrawler
 
 @author: SaFteiNZz
 """
-#Documentación relevante
-#https://docs.python.org/3/library/concurrent.futures.html
-#https://realpython.com/intro-to-python-threading/#starting-a-thread
-#https://www.tutorialsteacher.com/python/public-private-protected-modifiers
-#https://docs.python.org/3/library/urllib.parse.html
-#https://docs.python.org/3/library/queue.html
-#https://stackoverflow.com/questions/9371114/check-if-list-of-objects-contain-an-object-with-a-certain-attribute-value/9371143#9371143
+# =============================================================================
+#         ~DOC
+#
+#            https://docs.python.org/3/library/concurrent.futures.html
+#            https://realpython.com/intro-to-python-threading/#starting-a-thread
+#            https://www.tutorialsteacher.com/python/public-private-protected-modifiers
+#            https://docs.python.org/3/library/urllib.parse.html
+#            https://docs.python.org/3/library/queue.html
+#            https://stackoverflow.com/questions/9371114/check-if-list-of-objects-contain-an-object-with-a-certain-attribute-value/9371143#9371143
+#            https://doc.qt.io/archives/qt-4.8/qspinbox.html
+#
+# =============================================================================
+#         ~CONSTANTES
+# =============================================================================
 
+ICOENLACE = 'icono.png'
+GUIENLACE = 'interfazCrawling.ui'
 
-#Imports
+# =============================================================================
+#         ~IMPORTS
+# =============================================================================
+import sys
 from bs4 import BeautifulSoup
 import time, requests, re
 from queue import Queue, Empty
-#from threading import Thread
+from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
 from urllib.parse import urljoin, urlparse
- 
+
+#Datasets y models
+from pandasmodel import PandasModel
+import pandas as pd
+
+from qtpy.QtCore import Qt, QFileSystemWatcher, QSettings, Signal
+
+# --- Interfaz
+#Ventana
+from PyQt5 import uic
+#Messagebox
+import ctypes
+#PyQT5
+from PyQt5 import QtCore, QtGui, QtWidgets
+#Widgets pyQT5
+from PyQt5.QtWidgets import QMainWindow, QApplication
+#Importar interfaz
+Ui_MainWindow, QtBaseClass = uic.loadUiType(GUIENLACE)
+
+# =============================================================================
+#         ~FUNCIONES PUBLICAS
+# =============================================================================
+# ~Funcion alertas messagebox
+#
+#            @text comentario del messagebox
+#            @title titulo ventana messagebox
+#            @style (INT) tipo de ventana
+#                  0 : OK
+#                  1 : OK | Cancel
+#                  2 : Abort | Retry | Ignore
+#                  3 : Yes | No | Cancel
+#                  4 : Yes | No
+#                  5 : Retry | No 
+#                  6 : Cancel | Try Again | Continue
+#            
+# =============================================================================
+def Messagebox(text, title, style):
+    return ctypes.windll.user32.MessageBoxW(0, text, title, style)  
+
+# -----------------------------------------------------------------------------
+# ~Clase enlace
+# =============================================================================
 class Enlace:
     def __init__(self, url, texto):
         self.url = url
         self.texto = texto
 
+# -----------------------------------------------------------------------------
+# ~Clase Crawler 
+# =============================================================================
 class CrawlerConcurrente:
     def __init__(self, url_scrap, termino):
         urlParsed = urlparse(url_scrap)
-        self.parametro_url = '{}://{}'.format(urlParsed.scheme, urlParsed.netloc)
-        self.aBuscar = termino
+        self.parametro_url = "{}://{}".format(urlParsed.scheme, urlParsed.netloc)
+        self.aBuscar = termino 
         self.enlacesEncontrados = [] #Lista de coincidencias
         
         self.colaCrawl = Queue(20)
@@ -62,44 +118,126 @@ class CrawlerConcurrente:
         if result and result.status_code == 200:
             self.BuscarCoincidencias(result.text)
                    
-    def runCC(self):
-        outFile = open("Coincidencias.txt","w")
-        print("Buscando el termino '" + self.aBuscar + "' en " + self.parametro_url)
+    def runCC(this, self):
+        print("Buscando el termino '" + this.aBuscar + "' en " + this.parametro_url)
         done = False
-        while not done:
+        while not done:            
             try:
-                enlace = self.colaCrawl.get(timeout=5)                
-                if any(x.url == enlace.url for x in self.enlacesEncontrados): continue 
+                enlace = this.colaCrawl.get(timeout=5)                
+                if any(x.url == enlace.url for x in this.enlacesEncontrados): continue 
             
-                if len(self.enlacesEncontrados) == 0:
-                    outputText = "Resultados de la busqueda del termino '" + self.aBuscar + "' en la pagina " + self.parametro_url + ":\n\n\n"
-                else:
-                    outputText = "Coincidencia nº " + str(len(self.enlacesEncontrados)) + "\nTexto: " + enlace.texto +"\nURL: " + enlace.url + "\n\n"
-                    print(outputText)
+                if len(this.enlacesEncontrados) > 0:                 
+                    df = pd.DataFrame([[enlace.texto,enlace.url]], columns=('Enlace', 'URL'))
+                    self.updateProgress.emit(df)
                 
-                outFile.write(outputText)
-                self.enlacesEncontrados.append(enlace)
+                this.enlacesEncontrados.append(enlace)
                 
-#                workers
-                worker = self.workers.submit(self.scrapWeb, enlace.url)
-                worker.add_done_callback(self.scrapeCallback)
-                time.sleep(0.5)  
+#                Scrap workers
+                worker = this.workers.submit(this.scrapWeb, enlace.url)
+                worker.add_done_callback(this.scrapeCallback)
+                time.sleep(0.5)
+                
+#                Limitar coincidencias
+                if self.ui.cBLimitarCoincidencias.isChecked():
+                    if len(this.enlacesEncontrados) - 1 == self.ui.sBCoincidencias.value():
+                        raise Empty()
                     
             except Empty:
-                print("Busqueda completada")
-                done = True
+                self.ui.lEstadoActual.setText('Busqueda finalizada')
+                done = True                
                 return
             
             except Exception as e:
                 print(e)
                 continue
-        
-#        Cerrar fichero cuando termine cola 
-        outFile.close()
-        
+            
+# -----------------------------------------------------------------------------
+# ~Clase ventana
+# =============================================================================
+class mainClass(QMainWindow):
     
+    updateProgress = Signal(pd.DataFrame)
+    
+    def __init__(self):
+        super(mainClass, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self) 
+        self.setWindowIcon(QtGui.QIcon(ICOENLACE))
         
+        df = pd.DataFrame(columns=('Enlace', 'URL'))    
+        self.model = PandasModel(df)
+        self.ui.tVResultado.setModel(self.model)
+        self.ui.tVResultado.setColumnWidth(0, 400)
+        self.ui.tVResultado.horizontalHeader().setStretchLastSection(True)
+        self.updateProgress.connect(self.model.add)
+        
+# 
+#         ~Eventos links
+# =============================================================================
+        
+# ---------- Botones
+        #Boton Buscar
+        self.ui.pBBuscar.clicked.connect(self.pBBuscarClicked)
+        #Boton Exportar
+        self.ui.pBExportar.clicked.connect(self.pBExportarClicked)  
+
+# ---------- Otros 
+        #Line edit URL
+        self.ui.tETermino.mousePressEvent = self.tETerminoClicked
+        #Line edit URL
+        self.ui.tEURLPagina.mousePressEvent = self.tEURLPaginaClicked 
+        #Checkbox coincidencias
+        self.ui.cBLimitarCoincidencias.stateChanged.connect(self.cBLimitarCoincidenciasChanged)
+
+        
+# 
+# ~Funciones eventos
+# =============================================================================
+#     ~Evento Buscar coincidencias  
+# =============================================================================
+    def pBBuscarClicked( self ):
+        termino = self.ui.tETermino.toPlainText()
+        url = self.ui.tEURLPagina.toPlainText()
+        cc = CrawlerConcurrente(url,termino)
+        self.ui.lEstadoActual.setText('Buscando...')
+        mainWorker = Thread(target=cc.runCC, args=(self, ))
+        mainWorker.start()
+#        cc.runCC(self)
+        
+# =============================================================================
+#     ~Evento Exportar coincidencias  
+# =============================================================================
+    def pBExportarClicked( self ):
+        print("sin hacer")
+        
+# =============================================================================
+#     ~Evento checkbox limitar coincidencias 
+# =============================================================================       
+    def cBLimitarCoincidenciasChanged( self ):
+        if self.ui.sBCoincidencias.isEnabled(): self.ui.sBCoincidencias.setDisabled(True)
+        else: self.ui.sBCoincidencias.setEnabled(True)
+        
+# =============================================================================
+#     ~Eventos limpiar textedits onclick  (se tiene que poder mandar el widget como argumento y no duplicamos codigo)
+# =============================================================================
+    def tEURLPaginaClicked(self, event):
+        self.ui.tEURLPagina.setText('')
+        self.ui.tEURLPagina.setTextColor(QtGui.QColor(0,0,0))
+        
+    def tETerminoClicked(self, event):
+        self.ui.tETermino.setText('')
+        self.ui.tETermino.setTextColor(QtGui.QColor(0,0,0))
+#   
+# RUNUP
+# =============================================================================
+# *Función MAIN
+###############################################################################
+def main():
+    app = QApplication(sys.argv)
+    window = mainClass()
+    window.show()
+    app.exec_()
+# *Ejecucion ------------------------------------------------------------------
 if __name__ == '__main__':
-    cc = CrawlerConcurrente("https://www.elpais.com/", "vacuna")
-#    cc = CrawlerConcurrente("https://www.20minutos.es/", "vacuna")
-    cc.runCC()
+    main()   
+#------------------------------------------------------------------------------
